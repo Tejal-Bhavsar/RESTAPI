15c64e45bd93076e4d08c890b9f4d38792e64a23
tejal-bhavsar <tejalbhavsar02@gmail.com>
2024-03-20 14:36:18 +0530

    created routes for all types of request

--- index.js
+++ index.js
@@ -17,6 +17,28 @@ app.get('/api/users', (req, res) => {
     return res.send(html)
 })
 
+// dynamic path params
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
+})
+
+app.post('/api/users/', (req, res) => {
+    // create new  user 
+    return res.json({staus: 'pending'})
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    // edit user wid perticular id
+    return res.json({staus: 'pending'})
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    // delte user wid perticular id
+    return res.json({staus: 'pending'})
+})
+
 app.listen(PORT, () => {
     console.log('I am a server at port 8000')
 })
